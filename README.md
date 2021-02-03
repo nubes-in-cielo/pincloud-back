8bc7fe42e89f73102de3676e52b76642288d4383
parkoon <devparkoon@gmail.com>
2021-02-03 23:09:42 +0900

    feat: init project 🎉
    * todos
    - [ ] typeorm
    - [ ] alias
    - [ ] code file naming
    - [ ] refactor api folder
    - [ ] check working successfully

---
+++ README.md
@@ -0,0 +1,9 @@
+# pincloud backend
+
+🚧 기본 프로젝트 세팅중 🚧
+
+- [ ] typeorm
+- [ ] alias
+- [ ] code file naming
+- [ ] refactor api folder
+- [ ] check working successfully
---
+++ src/decorators/eventDispatcher.ts
@@ -0,0 +1,14 @@
+/**
+ * https://docs.typestack.community/typedi/v/develop/
+ */
+import { EventDispatcher as EventDispatcherClass } from 'event-dispatch'
+import { Container } from 'typedi'
+
+export function EventDispatcher() {
+  return (object: any, propertyName: string, index?: number): void => {
+    const eventDispatcher = new EventDispatcherClass()
+    Container.registerHandler({ object, propertyName, index, value: () => eventDispatcher })
+  }
+}
+
+export { EventDispatcher as EventDispatcherInterface } from 'event-dispatch'
